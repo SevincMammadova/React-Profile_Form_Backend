054fb4a20456b4ca334abcb65b25c867e6f60936
SevincMammadova <sevincmammadova609@gmail.com>
2020-05-22 03:05:52 +0400

    PFB-2 create request, task done

--- src/Controller.js
+++ src/Controller.js
@@ -2,6 +2,10 @@ class Controller{
     constructor(model) {
         this.model = model;
     }
+
+    addObjToModel(data) {
+        this.model.addElement(data);
+    }
 }
 
 module.exports = Controller;
